f81abafc88a80ad276ee48500271a4f20eb5cd56
[Aureb29] <[aurelien.bertini92@gmail.com]>
2018-10-15 15:29:20 +0200

    Update on file through~.c wich as been successfully edited to output a signal given in input

--- through~.c
+++ through~.c
@@ -5,50 +5,42 @@ static t_class *through_tilde_class;
 typedef struct _through_tilde {
 	
 	t_object	x_obj;
-	
-	t_float		f;
-	
-	t_outlet	*out_sig;
+	int		x_n;
+	t_float		x_f;
 	
 } t_through_tilde;
 
-t_int *through_tilde_perform(t_int *w){
+static t_int *through_tilde_perform(t_int *w){
 	
-	t_through_tilde *x	= (t_through_tilde *)(w[1]);
-	t_sample *in		= (t_sample *)(w[2]);
-	t_sample *out 		= (t_sample *)(w[3]);
-	int n 			= (int)(w[4]);
+	t_sample *in		= (t_sample *)(w[1]);
+	t_sample *out 		= (t_sample *)(w[2]);
+	int n 			= (int)(w[3]);
 	
 	while (n--) *out++ =  *in++;
 	
-	return (w+5);
+	return (w+4);
 }
 
 void through_tilde_dsp(t_through_tilde *x, t_signal **sp){
 	
-	dsp_add(through_tilde_perform, 4, x,
-		sp[0]->s_vec, sp[2]->s_vec, sp[0]->s_n);
+	dsp_add(through_tilde_perform, 3,
+		sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
 }
 
-void through_tilde_free(t_through_tilde *x){
-	
-	outlet_free(x->out_sig);
-}
-
-void *through_tilde_new(void){
+static void *through_tilde_new(t_floatarg f){
 	
 	t_through_tilde *x = (t_through_tilde *)pd_new(through_tilde_class);
+	x->x_n = f;
+	x->x_f = 0;
+	outlet_new(&x->x_obj, gensym("signal"));
 	
-	x->out_sig = outlet_new(&x->x_obj, &s_signal);
-	
-	return (void *)x;
+	return x;
 }
 
 void through_tilde_setup(void){
 	
 	through_tilde_class = class_new(gensym("through~"),
 					(t_newmethod)through_tilde_new,
-					(t_method)through_tilde_free,
 					0,
 					sizeof(t_through_tilde),
 					CLASS_DEFAULT,
@@ -58,9 +50,8 @@ void through_tilde_setup(void){
 	class_addmethod(through_tilde_class, 
 			(t_method)through_tilde_dsp, 
 			gensym("dsp"), 
-			A_CANT,
 			0);
-	CLASS_MAINSIGNALIN(through_tilde_class, t_through_tilde, f);
+	CLASS_MAINSIGNALIN(through_tilde_class, t_through_tilde, x_f);
 
 	class_sethelpsymbol(through_tilde_class, gensym("through~"));
 }
